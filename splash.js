e0023818bedf9538b1263ff8b5cd1417f8724105
Nhoco <gabrielpjrtgames@gmail.com>
2024-07-28 20:46:00 -0300

    first commit

---
+++ splash.js
@@ -0,0 +1,22 @@
+  const splash = [
+    "I make stuff",
+    "E",
+    "Can't belive this is butter!",
+    "title",
+    "placeholder",
+    "lol",
+    "made with <3",
+    "PHP is hard!",
+    "youtube is a youtube",
+    "CPU AT 100%",
+    "I'm a splash text!",
+    "Frick!",
+    "Neocities Rules!",
+    "i forgot to add something here",
+    "We must peform a quirkafleeg!",
+    "Hello, World!",
+    "404: Splash text not found",
+    "[insert something cool here]"
+  ];
+  var x = Math.floor((Math.random() * splash.length));
+  document.getElementById("splashtxt").innerHTML = splash[x];
