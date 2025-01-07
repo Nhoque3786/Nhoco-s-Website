1f46a3075f07e4ee6882daca7398cfd4ff282f52
TheDiamondfinderNG <tdf0926@duck.com>
2025-01-07 16:06:01 -0500

    Fix checkerboard

--- js/checkerground.js
+++ js/checkerground.js
@@ -5,29 +5,40 @@ document.addEventListener('DOMContentLoaded', () => {
     //get params
     const color1 = canvas.getAttribute('color1') || 'FF00FF';
     const color2 = canvas.getAttribute('color2') || '000000';
-    const anix = canvas.getAttribute('anix') || 0;
-    const animy = canvas.getAttribute('animy') || 0;
-    // note: it didint got the params so they are set up directly into the consts. need to fix this later!
+    const animX = parseFloat(canvas.getAttribute('animX')) || 1;
+    const animY = parseFloat(canvas.getAttribute('animY')) || 1;
+    const animation = canvas.getAttribute('animation') != "false";
+    // note: it didn't get the params so they are set up directly into the consts. need to fix this later!
 
     //get size of canvas
     const width = canvas.width;
     const height = canvas.height;
+    
+    // Animation offsets
+    let offsetX = 0,
+        offsetY = 0
 
     //draw checkerboard
     function drawCheckerboard() {
         const numSquares = 16; // per width
         const tileSize = width / numSquares;
-        for (let y = 0; y < height; y += tileSize) {
-            for (let x = 0; x < width; x += tileSize) {
+        for (let y = -64; y < height + 64; y += tileSize) {
+            for (let x = -64; x < width + 64; x += tileSize) {
                 ctx.fillStyle = (Math.floor(x/tileSize) + Math.floor(y/tileSize)) % 2 === 0 
                     ? `#${color1}` 
                     : `#${color2}`;
-                ctx.fillRect(x, y, tileSize, tileSize);
+                ctx.fillRect(x+offsetX, y+offsetY, tileSize, tileSize);
             }
+        }
+        if(animation){
+            animateCheckerboard()
         }
     }
     //animate this (if has params)
     function animateCheckerboard() { //do this shit later
+        offsetX = (offsetX+animX) % 64
+        offsetY = (offsetY+animY) % 64
+        requestAnimationFrame(drawCheckerboard)
     }
     drawCheckerboard();
 });
